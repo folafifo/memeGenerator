3a14eb1a9aef85cba72544bb50f54a8e28ece7fd
Fola Fifo <fola.fifo@propylon.com>
2022-03-03 13:10:24 +0000

    Lunch

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,11 +1,23 @@
 export default function Form() {
+  function handleClick() {
+    console.log("CLICKED");
+  }
+  function Hovered() {
+    console.log("HOVER");
+  }
   return (
     <div className="form">
       <span className="inputs">
         <input className="topInput" placeholder="Top Line" />
-        <input className="bottomInput" placeholder="Bottom Line" />
+        <input
+          onMouseEnter={Hovered}
+          className="bottomInput"
+          placeholder="Bottom Line"
+        />
       </span>
-      <button className="generate">Get a new meme image 😂</button>
+      <button onClick={handleClick} className="generate">
+        Get a new meme image 😂
+      </button>
     </div>
   );
 }
