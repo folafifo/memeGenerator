111846a23ab81c61a6b04215d88b46d0c3092080
Fola Fifo <fola.fifo@propylon.com>
2022-03-11 14:37:55 +0000

    update

--- src/components/Form.js
+++ src/components/Form.js
@@ -15,18 +15,44 @@ export default function Form() {
       ...prevMeme,
       imgUrl: url,
     }));
+    console.log(meme);
+  }
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setMeme((prevMeme) => {
+      return {
+        ...prevMeme,
+        [name]: value,
+      };
+    });
   }
 
   return (
     <div className="form">
       <span className="inputs">
-        <input className="topInput" placeholder="Top Line" />
-        <input className="bottomInput" placeholder="Bottom Line" />
+        <input
+          className="topInput"
+          placeholder="Top Line"
+          name="topText"
+          value={meme.topText}
+          onChange={handleChange}
+        />
+        <input
+          className="bottomInput"
+          placeholder="Bottom Line"
+          name="bottomText"
+          value={meme.bottomText}
+          onChange={handleChange}
+        />
       </span>
       <button onClick={handleClick} className="generate">
         Get a new meme image 😂
       </button>
-      <img src={meme.imgUrl} alt="" className="image" />
+      <div className="meme">
+        <img src={meme.imgUrl} alt="" className="image" />
+        <h2 className="meme--text top">{meme.topText}</h2>
+        <h2 className="meme--text bottom">{meme.bottomText}</h2>
+      </div>
     </div>
   );
 }
